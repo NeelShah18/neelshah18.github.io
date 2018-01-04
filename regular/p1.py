f3d6295684524c66ae52d8762c7d2b60b36b28e2
Neel Shah <neelknightme@gmail.com>
2018-01-04 13:58:49 -0500

    solution of problem 1

--- regular/p1.py
+++ regular/p1.py
@@ -1,3 +1,15 @@
+"""
+This problem was asked by Google.
+
+Given a stack of N elements, interleave the first half of the stack with the second half reversed using only one other queue. This should be done in-place.
+Recall that you can only push or pop from a stack, and enqueue or dequeue from a queue.
+
+For example, if the stack is [1, 2, 3, 4, 5], it should become [1, 5, 2, 4, 3]. If the stack is [1, 2, 3, 4], it should become [1, 4, 2, 3].
+
+Hint: Try working backwords from the end state.
+"""
+
+#Queue implementation
 class Queue:
     def __init__(self):
         self.queue = []
@@ -17,6 +29,7 @@ class Queue:
     def is_empty(self):
         return self.size() == 0
 
+#class implementation
 class Stack:
     def __init__(self):
         self.stack = []
@@ -41,8 +54,12 @@ class Stack:
 
     def is_empty(self):
         return self.size() == 0
+    
+    def pr(self):
+        return self.stack
 
-if __name__=="__main__":
+#solution
+def solution():
     data = [1,2,3,4]
     stack = Stack()
     q = Queue()
@@ -50,14 +67,23 @@ if __name__=="__main__":
     stack.push(2)
     stack.push(3)
     stack.push(4)
+    stack.push(5)
     flag = True
-    while flag == True:
-        mid = int(int(stack.size())/2)
-        temp = mid
-        print(mid)
-        while stack.size() > 1:
-            q.enqueue(stack.pop())
-            temp = temp-1
-        print(stack.size())
-        print(q.size())
-        flag=False
+    y=1
+    count = 1
+    while count < 4:
+        while stack.size() > y:
+            temp = stack.pop()
+            q.enqueue(temp)
+        
+        while q.size() >= 1:
+            temp2 = q.dequeue()
+            stack.push(temp2)
+        
+        y= y+1
+        count = count + 1
+    print(stack.pr())
+    return None
+
+if __name__=="__main__":
+    solution()
