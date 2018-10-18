31deeb76f16e5b1b9b8abf03bd3b282f155d9889
Neel Shah <neelknightme@gmail.com>
2018-10-17 21:27:52 -0400

    change in regular folder to luwork

--- regular/leetcode/3.py
+++
@@ -1,8 +0,0 @@
-import re
-def main():
-    s = ".314"
-    return bool(re.match("^\s*[\+-]?((\d+(\.\d*)?)|\.\d+)([eE][\+-]?\d+)?\s*$", s))
-
-
-if __name__=="__main__":
-    print(main())
--- regular/leetcode/8.py
+++
@@ -1,5 +0,0 @@
-
-n="01100110010"
-b = '{:0{width}b}'.format(n, width=width)
-ans=int(b[::-1], 2)
-print(ans)
